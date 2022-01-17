08c135c0065b31a3a4f2c4829df97379261b6ae9
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2022-01-17 19:23:08 -0300

    Mapeando entidades, criando repositórios e services para multiplas entidades

---
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -0,0 +1,55 @@
+package com.ead.course.models;
+
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CourseStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Entity
+@Table(name = "TB_COURSES")
+public class CourseModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID courseId;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 250)
+    private String description;
+
+    @Column
+    private String imageUrl;
+
+    @CreationTimestamp
+    @Column(name="creation_date", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private OffsetDateTime creationDate;
+
+    @UpdateTimestamp
+    @Column(name="last_update_date", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private OffsetDateTime lastUpdateDate;
+
+    @Enumerated(EnumType.STRING)
+    private CourseStatus courseStatus;
+
+    @Enumerated(EnumType.STRING)
+    private CourseLevel courseLevel;
+
+    @Column(nullable = false)
+    private UUID userInstructor;
+}
