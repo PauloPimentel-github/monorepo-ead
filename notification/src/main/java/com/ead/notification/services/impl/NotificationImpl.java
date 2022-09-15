0a9ab82830fc8ea1afbf4dfe0ddf8ee7ad79ff77
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-09-15 20:33:50 -0300

    Asynchronous Communication via Command Messages - Criar Exchange Topic, definir Queue e Bindings

--- notification/src/main/java/com/ead/notification/services/impl/NotificationImpl.java
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationImpl.java
@@ -1,5 +1,6 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,11 @@ public class NotificationImpl implements NotificationService {
 
     @Autowired
     private NotificationRepository notificationRepository;
+
+    @Override
+    public void saveNotification(NotificationModel notificationModel) {
+        this.notificationRepository.save(notificationModel);
+    }
 }
+
+
