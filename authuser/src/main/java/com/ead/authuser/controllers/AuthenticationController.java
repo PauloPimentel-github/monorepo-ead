7e16390fa362f3349a25e7369ebf7bf4a6a08b4d
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-09-20 19:10:45 -0300

    Authentication e Authorization com Basic Authentication  - Users Roles com Encoder Password em AuthUser

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -1,9 +1,12 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.UserDto;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.log4j.Log4j2;
@@ -11,6 +14,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +28,13 @@ import java.time.ZoneId;
 public class AuthenticationController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
@@ -40,12 +50,18 @@ public class AuthenticationController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: E-mail is Already Taken!");
         }
 
+        RoleModel roleModel = this.roleService.findByRoleName(RoleType.ROLE_STUDENT)
+                .orElseThrow(() -> new RuntimeException("Error: Role is Not Found."));
+
+        userDto.setPassword(this.passwordEncoder.encode(userDto.getPassword()));
+
         var userModel = new UserModel();
         BeanUtils.copyProperties(userDto, userModel);
         userModel.setUserStatus(UserStatus.ACTIVE);
         userModel.setUserType(UserType.STUDENT);
         userModel.setCreationDate(OffsetDateTime.now(ZoneId.of("UTC")));
         userModel.setLastUpdateDate(OffsetDateTime.now(ZoneId.of("UTC")));
+        userModel.getRoles().add(roleModel);
         userModel = this.userService.saveUser(userModel);
         log.debug("POST registerUser userId saved: {} ", userModel.getUserId());
         log.info("User saved successfully userId: {} ", userModel.getUserId());
