7a320d2c84563f055372dd7b10b81928bb6579aa
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-09-29 20:24:57 -0300

    Cross-Cutting Authentication e Authorization com JWT em Microservices - Authorization Roles para Instructor

--- course/src/main/java/com/ead/course/validation/CourseValidator.java
+++ course/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,11 +1,13 @@
 package com.ead.course.validation;
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.enums.UserType;
 import com.ead.course.models.UserModel;
 import com.ead.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -23,6 +25,9 @@ public class CourseValidator implements Validator {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationCurrentUserService authenticationCurrentUserService;
+
     @Override
     public boolean supports(Class<?> aClass) {
         return false;
@@ -38,14 +43,20 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        Optional<UserModel> userModelOptional = this.userService.findById(userInstructor);
-        if (userModelOptional.isEmpty()) {
-            errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found.");
-            return;
-        }
+        UUID currentId = this.authenticationCurrentUserService.getCurrentUser().getUserId();
 
-        if (userModelOptional.get().getUserType().compareTo(UserType.STUDENT.toString()) == 0) {
-            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+        if (currentId.compareTo(userInstructor) == 0) {
+            Optional<UserModel> userModelOptional = this.userService.findById(userInstructor);
+            if (userModelOptional.isEmpty()) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found.");
+                return;
+            }
+
+            if (userModelOptional.get().getUserType().compareTo(UserType.STUDENT.toString()) == 0) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+            }
+        } else {
+            throw new AccessDeniedException("Forbidden");
         }
     }
 }
